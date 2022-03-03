b6ed579841ce46825eb2757e3be7d768acac3d5e
AhobilamVenkatesh <100669340+AhobilamVenkatesh@users.noreply.github.com>
2022-03-03 18:19:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
  public static void main(String[] args){
 	System.out.println("Hello Albin, welcome to java world");
         System.out.println("Hello My Name is Venkatesh");
+	 int a=10;
  }
 
 }
